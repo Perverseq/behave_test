7d8012e158705370566be73a01caef9b82d74112
Perverseq <luchniknya@gmail.com>
2019-07-11 18:47:51 +0500

    Scenario 2 realized

--- features/environment.py
+++ features/environment.py
@@ -2,9 +2,6 @@ from selenium import webdriver
 import os
 
 
-
-
-
 def before_all(context):
     context.driver = webdriver.Chrome(os.path.abspath("chromedriver.exe"))
     context.driver.maximize_window()
--- features/steps/Scenario.py
+++ features/steps/Scenario.py
@@ -1,8 +1,11 @@
 from behave import *
-import time
+from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
+sites = {"google.ru": "https:\\www.google.ru",
+         "pw.mail.ru": "https:\\pw.mail.ru"}
 
 buttons = {"Новости": "//*[@id='menu']/li[1]/a",
            "Об игре": "//*[@id='menu']/li[2]/a",
@@ -16,9 +19,19 @@ content = {"Новости": "Все новости",
            "Скачать": "Системные требования",
            "Сообщество": "Поделки"}
 
-@when('Зашли на сайт "google.ru"')
-def step_impl(context):
-    context.driver.get("https:\\www.google.ru")
+buttons_sc2 = {"ВОЙТИ": "//*[@class='enter']/a[@value='Войти']",
+               "Войти": "//*[@class='gmrSignin__actions']/button[@id='js_kit_signin__submit']"}
+
+fields = {"E-mail": "//*[@name='Login']",
+          "Пароль": "//*[@name='Password']"}
+
+data = {"Логин": "login",
+        "Пароль": "password"}
+
+
+@when('Зашли на сайт "{text}"')
+def step_impl(context, text):
+    context.driver.get(sites[text])
 
 
 @step("Убедились, что появилось поисковое поле")
@@ -44,7 +57,6 @@ def step_impl(context, text):
     context.driver.switch_to_window(window_after)
 
 
-
 @then('Нажали на пункт "{text}"')
 def step_impl(context, text):
     WebDriverWait(context.driver, 5).until(EC.element_to_be_clickable((By.XPATH, buttons[text])))
@@ -56,67 +68,30 @@ def step_impl(context, text):
     assert content[text] in context.driver.page_source
 
 
-@when('Зашли на сайт "pw.mail.ru"')
+@step('Нажали кнопку "{text}"')
+def step_impl(context, text):
+    context.driver.find_element_by_xpath(buttons_sc2[text]).click()
+
+
+@then('Нашли поле "{text}" и ввели "{text1}"')
+def step_impl(context, text, text1):
+    context.driver.find_element_by_xpath(fields[text]).send_keys(data[text1])
+
+@then('Нажали на кнопку "Пополнить счет"')
 def step_impl(context):
-    """
-    :type context: behave.runner.Context
-    """
-    raise NotImplementedError(u'STEP: When Зашли на сайт "pw.mail.ru"')
+    WebDriverWait(context.driver, 3).until(EC.element_to_be_clickable((By.XPATH, "//*[@class='bill_link hv']")))
+    context.driver.find_element_by_xpath("//*[@class='bill_link hv']").click()
+    context.driver.switch_to.frame(context.driver.find_element_by_id('payment_iframe'))
+    WebDriverWait(context.driver, 3).until(EC.presence_of_element_located((By.XPATH, "//*[@id='xform-bonus']")))
 
 
-@step('Нажали кнопку "Войти"')
-def step_impl(context):
-    """
-    :type context: behave.runner.Context
-    """
-    raise NotImplementedError(u'STEP: And Нажали кнопку "Войти"')
+@then('Выбрали в выпадающем списке пункт со значением "{text}"')
+def step_impl(context, text):
+    Select(context.driver.find_element_by_xpath('//*[@id="xform-bonus"]')).select_by_value(text)
 
 
-@then('Нашли поле "E-mail"')
-def step_impl(context):
-    """
-    :type context: behave.runner.Context
-    """
-    raise NotImplementedError(u'STEP: Then Нашли поле "E-mail"')
-
-
-@step("Ввели логин")
-def step_impl(context):
-    """
-    :type context: behave.runner.Context
-    """
-    raise NotImplementedError(u'STEP: And Ввели логин')
-
-
-@then('Нашли поле "Пароль"')
-def step_impl(context):
-    """
-    :type context: behave.runner.Context
-    """
-    raise NotImplementedError(u'STEP: Then Нашли поле "Пароль"')
-
-
-@step("Ввели пароль")
-def step_impl(context):
-    """
-    :type context: behave.runner.Context
-    """
-    raise NotImplementedError(u'STEP: And Ввели пароль')
-
-
-@then('Выбрали в выпадающем списке пункт со значением ""')
-def step_impl(context):
-    """
-    :type context: behave.runner.Context
-    """
-    raise NotImplementedError(u'STEP: Then Выбрали в выпадающем списке пункт со значением ""')
-
-
-@step('Убедились, что значение равно ""')
-def step_impl(context):
-    """
-    :type context: behave.runner.Context
-    """
-    raise NotImplementedError(u'STEP: And Убедились, что значение равно ""')
-
+@step('Убедились, что значение в поле "Сумма платежа" равно "{text}"')
+def step_impl(context, text):
+    WebDriverWait(context.driver, 1).until(EC.text_to_be_present_in_element_value((By.ID, "xform-sum"), text))
+    assert text in context.driver.find_element_by_id("xform-sum").get_attribute('value')
 
